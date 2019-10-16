7476893af31ede1713a730a9e22edcccd77b7dfe
Lars Juhl <larsuldalljuhl@gmail.com>
2019-10-16 11:57:25 +0200

    Added time functionality + TurnServoOn

--- Software/firmware/src/firmwareArgon.ino
+++ Software/firmware/src/firmwareArgon.ino
@@ -1,8 +1,8 @@
 /*
- * Project firmware
+ * Project Smart home (Argon)
  * Description: IOT Project (Automatic morning routine)
  * Author: Lars Juhl  
- * Date:
+ * Date: 16/10/2019
  */
 
 // Declare variables
@@ -17,22 +17,27 @@ int analogTempValue = 0;        // Anolog temperature value
 int adc_value = 0;
 double tempVariable = 0;
 int actualTime = 0;
+int count = 0;
 String temp;
+
+int hour = 0; 
 
 // Initial setup
 void setup() {
+    Serial.begin(9600);
     pinMode(ledPin, OUTPUT);      // declare LED as OUTPUT
     pinMode(pirPin, INPUT);       // declare PIR sensor as input
     pinMode(tempRead, INPUT);     // declare tempSensor as input
 
     Particle.variable("PIR", PIRval);         // Cloud variable of PIR value
     Particle.variable("Temperature", tempVariable);  // Cloud variable of temp 
-    Particle.variable("Time", actualTime);
+    Particle.variable("Hour", hour);
 }
 
 // Program running
 void loop() 
 {
+  hour = Time.hour() + 2; // Stores the current hour (UTC) + 2 for local time
   actualTime = millis()/1000; // Secunds that the program has run
 
   // if the sensor is calibrated PIR detects motion
@@ -43,6 +48,26 @@ void loop()
 
     // report it out, if the state has changed
     reportThePIRData();
+
+      // Morning routine only runs once a day
+      if (hour >= 7 && hour < 12)
+        {
+           if(PIRval == 1 && count < 1)
+           {
+           Particle.publish("TurnServoOn", PRIVATE); // Event that the photon subscribes to
+           // Her kommer webhook til at tænde Hue lys
+           // Her kommer webhook til at tænde servoen fra photon
+           // Her kommer webhook til at tænde radioen
+           count = 1;
+           }
+ 
+        } else 
+          {
+          count = 0;
+          }
+        
+
+
     }
   
   // Get temperature
@@ -53,6 +78,7 @@ void loop()
           readTemperatureFunc();
       }
     }
+
 }
 
 
@@ -66,10 +92,6 @@ void readThePIRSensor() {
 
 bool calibratedPIR() {
     return millis() - calibrateTime > 0;  // When the calibration time is finished its True
-}
-
-void setLED(int state) {
-    digitalWrite(ledPin, state);    
 }
 
 void reportThePIRData() {
@@ -96,16 +118,21 @@ void reportThePIRData() {
     }
 }
 
+// LED lights up when motion is detected
+void setLED(int state) {
+    digitalWrite(ledPin, state);    
+}
+
 // Temperature sensor
 void readTemperatureFunc(){
     adc_value = analogRead(tempRead);           // Reads the adc value (max 1024 = 5V) 10mV/1deg C
     float tempValue = (adc_value*0.80)/10;      // Calculate to Celsius
     tempVariable = tempValue;
-    String temp = String(tempValue);            // Convert the Celsius result to a string
-    Particle.publish("temp", temp, PRIVATE);    // Publish an event named "temp" with the data of temp
+    String tempC = String(tempValue);            // Convert the Celsius result to a string
+    Particle.publish("temp", tempC, PRIVATE);    // Publish an event named "temp" with the data of tempC
 }
 
 bool modulusTime() {    
-    return (millis()/1000) % 60 == 0;     // Takes millisecunds the program has run, into sekunds, and divide with 60
-                                          // so it is only true every 60 sekunds
+    return (millis()/1000) % 10 == 0;     // Takes millisecunds the program has run, into sekunds, and divide with 10
+                                          // so it is only true every 10 sekunds
 }
